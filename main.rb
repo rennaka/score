90a4988f8a8e4c061d5990efd849cfe6de18380e
Dale <dale@dale.local>
2018-02-08 11:38:05 +0900

    for pull

---
+++ main.rb
@@ -0,0 +1,3 @@
+require "user.rb"
+user = User.new(1,"aaa",30)
+p user.name
---
+++ user.rb
@@ -0,0 +1,18 @@
+class User
+	attr_accessor :id, :name, :score
+
+	def initialize(id,name,score)
+		@id = id
+		@name = name
+		@score = score
+	end
+
+	def id
+	end
+
+	def name
+	end
+
+	def score
+	end
+end
